e3ae90c7f1ca65964b838bd88d8304cc2a1c35a8
Kelvin Muza <k1muza@gmail.com>
2025-06-29 21:53:19 +0200

    feat(admin): apply Comic Neue font

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,7 +1,12 @@
 
 import "@/app/globals.css";
 import { AnimalProvider } from "@/context/AnimalContext";
+import { Comic_Neue } from 'next/font/google';
 import type { ReactNode } from 'react';
+
+const comicNeue = Comic_Neue({
+  subsets: ['latin'],
+});
 
 export default function AdminLayout({
   children,
@@ -9,8 +14,10 @@ export default function AdminLayout({
   children: ReactNode;
 }>) {
   return (
+    <div className={comicNeue.className}>
       <AnimalProvider>
         {children}
       </AnimalProvider>
+    </div>
   );
 }
